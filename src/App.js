47a6519860105ece7a1bc2b5279ac82183170247
programmingwithbandhan <120212500+programmingwithbandhan@users.noreply.github.com>
2022-12-11 19:22:09 +0545

    changes to file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PasswordGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "",
+    };
+  }
+
+  generatePassword = () => {
+    // Generate a random password and set it to the "password" state property
+    this.setState({
+      password: Math.random().toString(36).slice(-8),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Password Generator</h1>
+        <button onClick={this.generatePassword}>Generate Password</button>
+        <p>Your password: {this.state.password}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default PasswordGenerator
